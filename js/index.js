3beb85e0c0411bd63762091ea0598dcc967ff4a9
unknown <sukie_huang@foxmail.com>
2015-01-20 12:05:17 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 茜 on 2014/11/29.
+ */
+
+$(function(){
+    //nav click
+    $(".navbar").removeClass("open");
+    $(".menu-button").on('click',function(){
+        $(".navbar").toggleClass("open");
+    })
+})
+
+
